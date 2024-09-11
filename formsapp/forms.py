dd74d59d7aebab48ce1f4f115d801f40ab64cd9e
Felipe Torres <ftorres@intralog.local>
2024-09-11 12:06:58 -0300

    realizados algunos cambios para la obligatoriedad de algunos campos en la carga manual de forms

--- formsapp/forms.py
+++ formsapp/forms.py
@@ -25,10 +25,19 @@ class ManualFormSubmissionForm(forms.ModelForm):
     empresa = forms.CharField(required=True)
     estado = forms.ChoiceField(choices=ESTADO_CHOICES, required=True)
     assigned_user = forms.ModelChoiceField(queryset=CustomUser.objects.all(), required=True)
+    razon_social = forms.CharField(required=False)
+    mail = forms.EmailField(required=False)
     
     # Optional fields
-    razon_social = forms.CharField(required=False)
     nombre_y_apellido = forms.CharField(required=False)
-    mail = forms.EmailField(required=False)
     telefono = forms.CharField(required=False)
     mensaje = forms.CharField(widget=forms.Textarea, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(ManualFormSubmissionForm, self).__init__(*args, **kwargs)
+
+        self.fields['empresa'].help_text = "obligatorio"
+        self.fields['estado'].help_text = "obligatorio"
+        self.fields['assigned_user'].help_text = "obligatorio"
+        self.fields['razon_social'].help_text = "obligatorio"
+        self.fields['mail'].help_text = "obligatorio"
